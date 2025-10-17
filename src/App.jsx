f08e58339eb9034dabc64b1b09346a245783250c
JalilovAbdurahmon <jalilovabdurahmon2010@gmail.com>
2025-10-17 19:56:45 +0500

    about done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Layout from './pages/Layout'
+import About from './pages/About'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path='/' element={<Layout />}>
-          
+          <Route path='/about' element={<About />} />
         </Route>
       </Routes>
     </div>
