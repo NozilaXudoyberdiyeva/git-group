abb5aafca55f382f48be4e3995fb857a4d22ada4
Your Name <you@example.com>
2025-10-17 19:23:41 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Layout from './pages/Layout'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+  return (
+    <div>
+      <h1>Logo</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contacts</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
